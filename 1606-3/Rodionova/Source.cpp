f7b0c6fc854a1f441ff2be666df4bbf0d86dac9b
Rodionova Daria <32214402+RodionovaDaria98@users.noreply.github.com>
2019-04-14 23:57:20 +0300

    Add files via upload
    Гитхаб не починился(

--- 1606-3/Rodionova/Source.cpp
+++ 1606-3/Rodionova/Source.cpp
@@ -1,203 +1,204 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE
-#define _CRT_NONSTDC_NO_DEPRECATE
-#pragma warning(disable : 4996)
+
 #include <iostream>
-#include <ctime>
-#include <iomanip>
-#include <stdio.h>
-#include <mpi.h>
-#include <time.h>
+#include <math.h>
+#include <omp.h>
 
-#define N 1000000
+#define Eps 0.01
 
-void showElapsed(int id, char *m);
-void showVector(int *v, int n, int id);
-int * merge(int *A, int asize, int *B, int bsize);
-void swap(int *v, int i, int j);
-void m_sort(int *A, int min, int max);
 
-double startT, stopT;
 
-double startTime;
+double val_one_dimensional_func(double x);
+double val_two_dimensonal_func(double x1, double x2);
 
-void showElapsed(int id, char *m)
-{
-	printf("%d: %s %f secs\n", id, m, (clock() - startTime) / CLOCKS_PER_SEC);
+void one_dimensional_integral(double a, double b, double h, double *res);
+void one_dim_integral_parallel(double a, double b, double h, double *res);
+
+void two_dimensional_integral(double a1, double b1, double a2, double b2, double h, double *res);
+void two_dim_integral_parallel(double a1, double b1, double a2, double b2, double h, double *res);
+void two_dimensional_integral_fix_point(double a1, double a2, double b2, double h, double *res);
+
+void two_dimensional_integral_multi_step(double a1, double b1, double a2, double b2, double h, double *res);
+void two_dim_integral_parallel_multi_step(double a1, double b1, double a2, double b2, double h, double *res);
+
+
+double val_one_dimensional_func(double x) {
+	return sin(x);
 }
 
-void showVector(int *v, int n, int id)
-{
+double val_two_dimensonal_func(double x1, double x2) {
+	return sin(x1) * cos(x2);
+}
+
+void one_dimensional_integral(double a, double b, double h, double *res) {
+	double sum = 0.0;
+	for (double i = a; i < b; i += h) {
+		sum += val_one_dimensional_func(i) * h;
+	}
+	*res = sum;
+}
+void one_dim_integral_parallel(double a, double b, double h, double *res) {
+	double total_res = 0.0;
 	int i;
-	printf("%d: ", id);
-	for (i = 0; i < n; i++)
-		printf("%d ", v[i]);
-	putchar('\n');
+
+#pragma omp parallel for private(i) reduction(+: total_res)
+	for (i = 0; i < int((b - a) / h); i++) {
+		total_res += val_one_dimensional_func(a + i * h) * h;
+	}
+
+	*res = total_res;
 }
 
-int * merge(int *A, int asize, int *B, int bsize) {
-	int ai, bi, ci, i;
-	int *C;
-	int csize = asize + bsize;
-
-	ai = 0;
-	bi = 0;
-	ci = 0;
-
-	/* printf("asize=%d bsize=%d\n", asize, bsize); */
-
-	C = (int *)malloc(csize * sizeof(int));
-	while ((ai < asize) && (bi < bsize)) {
-		if (A[ai] <= B[bi]) {
-			C[ci] = A[ai];
-			ci++; ai++;
-		}
-		else {
-			C[ci] = B[bi];
-			ci++; bi++;
+void two_dim_integral_parallel(double a1, double b1, double a2, double b2, double h, double *res) {
+	double sum = 0.0;
+	int i, j;
+#pragma omp parallel
+	{ 
+		
+#pragma omp for private(i, j) reduction(+: sum)
+		for (i = 0; i < int((b1 - a1) / h); i++)
+		{
+			for (j = 0; j < int((b2 - a2) / h); j++)
+			{
+				sum += val_two_dimensonal_func(a1 + i * h, a2 + j * h) * h * h;
+			}
 		}
 	}
-
-	if (ai >= asize)
-		for (i = ci; i < csize; i++, bi++)
-			C[i] = B[bi];
-	else if (bi >= bsize)
-		for (i = ci; i < csize; i++, ai++)
-			C[i] = A[ai];
-
-	for (i = 0; i < asize; i++)
-		A[i] = C[i];
-	for (i = 0; i < bsize; i++)
-		B[i] = C[asize + i];
-
-	/* showVector(C, csize, 0); */
-	return C;
+	*res = sum;
 }
+void two_dimensional_integral(double a1, double b1, double a2, double b2, double h, double *res) {
+	double sum = 0.0;
 
-void swap(int *v, int i, int j)
-{
-	int t;
-	t = v[i];
-	v[i] = v[j];
-	v[j] = t;
-}
-
-void m_sort(int *A, int min, int max)
-{
-	int *C;		/* dummy, just to fit the function */
-	int mid = (min + max) / 2;
-	int lowerCount = mid - min + 1;
-	int upperCount = max - mid;
-
-	/* If the range consists of a single element, it's already sorted */
-	if (max == min) {
-		return;
+	for (double i = a1; i < b1; i += h)
+	{
+		for (double j = a2; j < b2; j += h)
+		{
+			sum += val_two_dimensonal_func(i, j) * h * h;
+		}
 	}
-	else {
-		/* Otherwise, sort the first half */
-		m_sort(A, min, mid);
-		/* Now sort the second half */
-		m_sort(A, mid + 1, max);
-		/* Now merge the two halves */
-		C = merge(A + min, lowerCount, A + mid + 1, upperCount);
-	}
+	*res = sum;
 }
+
+void two_dimensional_integral_fix_point(double a1, double a2, double b2, double h, double *res) {
+	double sum = 0.0;
+
+	for (double j = a2; j < b2; j += h)
+	{
+		sum += val_two_dimensonal_func(a1, j) * h;
+	}
+
+	*res = sum;
+}
+
+void two_dim_integral_parallel_multi_step(double a1, double b1, double a2, double b2, double h, double *res, int p)
+{
+	double sum = 0.0;
+	double tmpsum = 0.0;
+	int i, j;
+#pragma omp parallel num_threads(p)
+	{
+	
+			
+#pragma omp for private(i, j, tmpsum) reduction(+: sum)
+		for (i = 0; i < int((b1 - a1) / h); i++)
+		{
+			two_dimensional_integral_fix_point(a1 + h * i, a2, b2, h, &tmpsum);
+			sum += tmpsum * h;
+		}
+	}
+	*res = sum;
+}
+
+void two_dimensional_integral_multi_step(double a1, double b1, double a2, double b2, double h, double *res) {
+	double sum = 0.0;
+	double tmpsum = 0.0;
+	int i;
+	for (i = 0; i < int((b1 - a1) / h); i++)
+	{
+		two_dimensional_integral_fix_point(a1 + h * i, a2, b2, h, &tmpsum);
+		sum += tmpsum * h;
+	}
+	*res = sum;
+}
+
+void two_dim_integral_parallel_multi_step(double a1, double b1, double a2, double b2, double h, double * res)
+{
+}
+
+
+
+
 
 int main(int argc, char **argv)
 {
-	int * data;
-	int * chunk;
-	int * other;
-	int m, n = N;
-	int id, p;
-	int s = 0;
-	int i;
-	int step;
-	MPI_Status status;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &id);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+	double a1 = 0.0, b1 = 5.0, a2 = 0.0, b2 = 5.0;
+	double h = 0.001;
+	double seqRes = 0.0, parRes = 0.0;
+	double t1 = 0.0, t2 = 0.0, parallTimeWork = 0.0, sequentTimeWork = 0.0;
 
-	startT = clock();
-	if (id == 0)
-	{
-		int r;
-		srand(clock());
-		s = n / p;
-		r = n % p;
-		data = (int *)malloc((n + s - r) * sizeof(int));
-		for (i = 0; i < n; i++)
-			data[i] = rand();
-		if (r != 0)
-		{
-			for (i = n; i < n + s - r; i++)
-				data[i] = 0;
-			s = s + 1;
+
+
+	int p = 4;
+	std::cout << "Set a number of threads" << std::endl;
+	std:: cin >> p;
+	omp_set_num_threads(p);
+
+
+
+
+	for (int i = 1; i < argc; ++i) {
+		if ((!strcmp(argv[i], "-a1")) && (i + 1 < argc)) {
+			a1 = atof(argv[i + 1]);
 		}
-
-
-		MPI_Bcast(&s, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		chunk = (int *)malloc(s * sizeof(int));
-		MPI_Scatter(data, s, MPI_INT, chunk, s, MPI_INT, 0, MPI_COMM_WORLD);
-		m_sort(chunk, 0, s - 1);
-		/* showVector(chunk, s, id); */
-	}
-	else
-	{
-		int r;
-		r = n % p;
-		data = (int *)malloc((n + s - r) * sizeof(int));
-		if (r != 0)
-		{
-			for (i = n; i < n + s - r; i++)
-				data[i] = 0;
-			s = s + 1;
+		if ((!strcmp(argv[i], "-b1")) && (i + 1 < argc)) {
+			b1 = atof(argv[i + 1]);
 		}
-		MPI_Bcast(&s, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		chunk = (int *)malloc(s * sizeof(int));
-		MPI_Scatter(data, s, MPI_INT, chunk, s, MPI_INT, 0, MPI_COMM_WORLD);
-		m_sort(chunk, 0, s - 1);
-		/* showVector(chunk, s, id); */
+		if ((!strcmp(argv[i], "-h")) && (i + 1 < argc)) {
+			h = atof(argv[i + 1]);
+		}
+		if ((!strcmp(argv[i], "-a2")) && (i + 1 < argc)) {
+			a2 = atof(argv[i + 1]);
+		}
+		if ((!strcmp(argv[i], "-b2")) && (i + 1 < argc)) {
+			b2 = atof(argv[i + 1]);
+		}
 	}
 
-	step = 1;
-	while (step < p)
-	{
-		if (id % (2 * step) == 0)
-		{
-			if (id + step < p)
-			{
-				MPI_Recv(&m, 1, MPI_INT, id + step, 0, MPI_COMM_WORLD, &status);
-				other = (int *)malloc(m * sizeof(int));
-				MPI_Recv(other, m, MPI_INT, id + step, 0, MPI_COMM_WORLD, &status);
-				chunk = merge(chunk, s, other, m);
-				s = s + m;
-			}
-		}
-		else
-		{
-			int near = id - step;
-			MPI_Send(&s, 1, MPI_INT, near, 0, MPI_COMM_WORLD);
-			MPI_Send(chunk, s, MPI_INT, near, 0, MPI_COMM_WORLD);
-			break;
-		}
-		step = step * 2;
+	if (a1 == INFINITY || b2 == INFINITY) {
+		
+		t1 = omp_get_wtime();
+		one_dimensional_integral(a1, b1, h, &seqRes);
+		t2 = omp_get_wtime();
+		sequentTimeWork = (t2 - t1) * 1000.;
+		std::cout << "Result of sequential algorithm = " << seqRes << std::endl;
+		std::cout << "sequentTimeWork = " << sequentTimeWork << " ms" << std::endl;
+
+		t1 = omp_get_wtime();
+		one_dim_integral_parallel(a1, b1, h, &parRes);
+		t2 = omp_get_wtime();
+		parallTimeWork = (t2 - t1) * 1000.;
+		std::cout << "Result of parallel algorithm = " << parRes << std::endl;
+		std::cout << "parallelTimeWork = " << parallTimeWork << " ms" << std::endl;
+
+	}
+	else {
+
+		t1 = omp_get_wtime();
+		two_dimensional_integral_multi_step(a1, b1, a2, b2, h, &seqRes);
+		t2 = omp_get_wtime();
+		sequentTimeWork = (t2 - t1) * 1000.;
+		std::cout << "Result of sequential algorithm = " << seqRes << std::endl;
+		std::cout << "sequentTimeWork = " << sequentTimeWork << " ms" << std::endl;
+
+		t1 = omp_get_wtime();
+		two_dim_integral_parallel_multi_step(a1, b1, a2, b2, h, &parRes);
+		t2 = omp_get_wtime();
+		parallTimeWork = (t2 - t1) * 1000.;
+		std::cout << "Result of parallel algorithm = " << parRes << std::endl;
+		std::cout << "parallelTimeWork = " << parallTimeWork << " ms" << std::endl;
+
+		
 	}
 
-	stopT = clock();
-	if (id == 0)
-	{
-		FILE * fout;
-
-		printf("%d; %d processors; %f secs\n", s, p, (stopT - startT) / CLOCKS_PER_SEC);
-
-		fout = fopen("result.txt", "w");
-		for (i = 0; i < s; i++)
-			fprintf(fout, "%d\n", chunk[i]);
-		fclose(fout);
-	}
-	MPI_Finalize();
 	system("pause");
-	return 0;
 }
